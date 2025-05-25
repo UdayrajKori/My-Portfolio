d0a77f95992c10d40ad328ca94bf915bc676a467
Aiko14510 <aasthasherchan78699@gmail.com>
2025-05-25 16:01:24 +0545

    Added the portfolio section and about contact section

--- script.js
+++ script.js
@@ -1,18 +1,64 @@
 const resumeBtn = document.querySelectorAll(".resume-btn");
 
-resumeBtn.forEach((btn, idx) =>{
-    btn.addEventListener('click', ()=>{
-        const resumeDetails = document.querySelectorAll(".resume-details");
+resumeBtn.forEach((btn, idx) => {
+  btn.addEventListener("click", () => {
+    const resumeDetails = document.querySelectorAll(".resume-details");
 
-        resumeBtn.forEach(btn =>{
-            btn.classList.remove('active');
-        });
-        btn.classList.add('active');
-
-        resumeDetails.forEach((detail) => {
-          detail.classList.remove("active");
-        });
-        resumeDetails[idx].classList.add("active");
-
+    resumeBtn.forEach((btn) => {
+      btn.classList.remove("active");
     });
+    btn.classList.add("active");
+
+    resumeDetails.forEach((detail) => {
+      detail.classList.remove("active");
+    });
+    resumeDetails[idx].classList.add("active");
+  });
+});
+
+const arrowRight = document.querySelector(
+  ".portfolio-box .navigation .arrow-right"
+);
+const arrowLeft = document.querySelector(
+  ".portfolio-box .navigation .arrow-left"
+);
+
+let index = 0;
+
+const activePortfolio = () => {
+  const imgSlide = document.querySelector(".portfolio-carousel .img-slide");
+  const portfolioDetails = document.querySelectorAll(".portfolio-details");
+
+  imgSlide.style.transform = `translateX(calc(${index * -100}% - ${
+    index * 2
+  }rem))`;
+
+  portfolioDetails.forEach(details =>{
+    details.classList.remove('active');
+  })
+  portfolioDetails[index].classList.add('active');
+};
+
+arrowRight.addEventListener("click", () => {
+  if (index < 4) {
+    index++;
+    arrowLeft.classList.remove('disabled');
+  } else {
+    index = 5;
+    arrowRight.classList.add('disabled');
+  }
+
+  activePortfolio();
+});
+
+arrowLeft.addEventListener("click", () => {
+  if (index > 1) {
+    index--;
+    arrowRight.classList.remove('disabled');
+  } else {
+    index = 0;
+    arrowLeft.classList.add('disabled');
+  }
+
+  activePortfolio();
 });

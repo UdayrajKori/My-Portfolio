42fcff55fec0f7c199f45a6598d40a9c3e15b35b
Aiko14510 <aasthasherchan78699@gmail.com>
2025-05-26 15:31:13 +0545

    Added the feature of linking all pages and also added the feature of responsive size code

--- script.js
+++ script.js
@@ -1,3 +1,67 @@
+const navLink = document.querySelectorAll('header nav a');
+const logoLink = document.querySelector('.logo');
+const sections = document.querySelectorAll('section');
+const menuIcon = document.querySelector("#menu-icon");
+const navbar = document.querySelector("header nav");
+
+
+menuIcon.addEventListener('click', () => {
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('active');
+});
+
+const activePage = () =>{
+
+  const barsBox = document.querySelector(".bars-box");
+  const header = document.querySelector('header');
+
+  header.classList.remove("active");
+  setTimeout(() => {
+    header.classList.add("active");
+  }, 1100);
+
+  navLink.forEach(link =>{
+    link.classList.remove('active');
+  });
+
+  barsBox.classList.remove('active');
+  setTimeout(() => {
+    barsBox.classList.add('active');
+  }, 1100);
+
+  sections.forEach((section) => {
+    section.classList.remove("active");
+  });
+
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
+
+}
+
+navLink.forEach((link, idx) =>{
+  link.addEventListener('click', () =>{
+    if (!link.classList.contains('active')){
+      activePage();
+      link.classList.add('active');
+
+      setTimeout(() => {
+        sections[idx].classList.add("active");
+      }, 1100);
+    }
+  });
+});
+
+logoLink.addEventListener('click', () => {
+  if (!navLink[0].classList.contains('active')) {
+    activePage();
+    navLink[0].classList.add('active');
+
+    setTimeout(() => {
+      sections[0].classList.add("active");
+    }, 1100);
+  }
+});
+
 const resumeBtn = document.querySelectorAll(".resume-btn");
 
 resumeBtn.forEach((btn, idx) => {
